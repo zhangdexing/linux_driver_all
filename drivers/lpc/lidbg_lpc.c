31574b1b55ea9ebd3e9c8f9dbc67e9b350462394
lisuwei <lisuweizhai@126.com>
2016-07-22 16:18:29 +0800

    修正G9 6.0 recovery跟lpc通讯问题

--- drivers/lpc/lidbg_lpc.c
+++ drivers/lpc/lidbg_lpc.c
@@ -616,7 +616,7 @@ static int  lpc_probe(struct platform_device *pdev)
         lidbg("lpc_init do nothing.disable,[%d,%d,%d,%d]\n", g_var.is_fly, lpc_ping_en, g_var.recovery_mode, fs_is_file_exist(FLY_HAL_FILE));
         return 0;
     }
-#if defined(PLATFORM_msm8974) || defined(PLATFORM_msm8228)
+#if defined(PLATFORM_msm8228) || defined(PLATFORM_ID_4) || defined(PLATFORM_ID_7)
 	  if(g_var.recovery_mode)
 	 		return 0;
 #endif
