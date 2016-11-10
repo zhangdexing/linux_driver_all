de09b1d4503bdc47ae0f01b2880c03f757cac6a1
LiangYiHong <363095394@qq.com>
2016-11-10 14:16:10 +0800

    行车记录仪修复BUG：双录设置后后录不录&&无版本信息问题

--- drivers/flycam/lidbg_flycam.c
+++ drivers/flycam/lidbg_flycam.c
@@ -436,7 +436,7 @@ static int thread_set_par_func(void *data)
 			msleep(800);
 			start_rec(REARVIEW_ID,1);
 		}
-#if 0
+#if 1
 		if(isDualCam)
 		{
 			if((pfly_UsbCamInfo->camStatus) & FLY_CAM_ISVALID)	isDVRVideoLoop = 1;
