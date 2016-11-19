d664dfb9c22f93bb7b811ebee3f76309dac3d178
futengfei <540720599@qq.com>
2016-11-19 10:47:27 +0800

    调整app编译顺序

--- app/Android.mk
+++ app/Android.mk
@@ -11,6 +11,17 @@ SUBDIR_MAKEFILES += $(call all-named-subdir-makefiles,apps)
 SUBDIR_MAKEFILES += $(call all-named-subdir-makefiles,lidbg_umount)
 SUBDIR_MAKEFILES += $(call all-named-subdir-makefiles,apk)
 
+ifeq ($(CONFIG_HAL_ANDROID_SERVER),y)
+SUBDIR_MAKEFILES += $(call all-named-subdir-makefiles,lidbg_android_server)
+endif
+
+ifeq ($(CONFIG_HAL_LOAD),y)
+SUBDIR_MAKEFILES += $(call all-named-subdir-makefiles,lidbg_load)
+endif
+
+ifeq ($(CONFIG_HAL_USERVER),y)
+SUBDIR_MAKEFILES += $(call all-named-subdir-makefiles,lidbg_userver)
+endif
 
 ifeq ($(CONFIG_HAL_GPSLIB),y)
 SUBDIR_MAKEFILES += $(call all-named-subdir-makefiles,gpslib)
@@ -24,16 +35,42 @@ ifeq ($(CONFIG_HAL_SERVER),y)
 SUBDIR_MAKEFILES += $(call all-named-subdir-makefiles,lidbg_servicer)
 endif
 
+ifeq ($(CONFIG_HAL_UART_SEND_DATA),y)
+SUBDIR_MAKEFILES += $(call all-named-subdir-makefiles,lidbg_uart_send_data)
+endif
+
+ifeq ($(CONFIG_HAL_PARTED),y)
+SUBDIR_MAKEFILES += $(call all-named-subdir-makefiles,lidbg_parted)
+endif
+
+ifeq ($(CONFIG_HAL_USB_CAMERA),y)
+SUBDIR_MAKEFILES += $(call all-named-subdir-makefiles,uvccam_test)
+SUBDIR_MAKEFILES += $(call all-named-subdir-makefiles,hal_camera_usb)
+SUBDIR_MAKEFILES += $(call all-named-subdir-makefiles,hal_camera_study_v4l2)
+endif
+
+ifeq ($(CONFIG_HAL_USB_CAMERA),y)
+SUBDIR_MAKEFILES += $(call all-named-subdir-makefiles,libusb01/android/jni)
+endif
+
+ifeq ($(CONFIG_HAL_USB_CAMERA),y)
+SUBDIR_MAKEFILES += $(call all-named-subdir-makefiles,camera-fw-update)
+endif
+
+ifeq ($(CONFIG_HAL_BOOTANIMATION),y)
+SUBDIR_MAKEFILES += $(call all-named-subdir-makefiles,bootanimation)
+endif
+
+ifeq ($(CONFIG_HAL_BOOTANIMATION_6),y)
+SUBDIR_MAKEFILES += $(call all-named-subdir-makefiles,bootanimation_6.0)
+endif
+
 ifeq ($(CONFIG_HAL_VOLD_8x25Q),y)
 SUBDIR_MAKEFILES += $(call all-named-subdir-makefiles,8x25q_vold)
 endif
 
 ifeq ($(CONFIG_HAL_CAMERA_8x25Q),y)
 SUBDIR_MAKEFILES += $(call all-named-subdir-makefiles,8x25q_camera)
-endif
-
-ifeq ($(CONFIG_HAL_USERVER),y)
-SUBDIR_MAKEFILES += $(call all-named-subdir-makefiles,lidbg_userver)
 endif
 
 ifeq ($(CONFIG_HAL_VOLD_8x26),y)
@@ -52,28 +89,6 @@ ifeq ($(CONFIG_HAL_VOLD_mt3360),y)
 SUBDIR_MAKEFILES += $(call all-named-subdir-makefiles,mt3360_vold)
 endif
 
-ifeq ($(CONFIG_HAL_LOAD),y)
-SUBDIR_MAKEFILES += $(call all-named-subdir-makefiles,lidbg_load)
-endif
-
-ifeq ($(CONFIG_HAL_UART_SEND_DATA),y)
-SUBDIR_MAKEFILES += $(call all-named-subdir-makefiles,lidbg_uart_send_data)
-endif
-
-ifeq ($(CONFIG_HAL_ANDROID_SERVER),y)
-SUBDIR_MAKEFILES += $(call all-named-subdir-makefiles,lidbg_android_server)
-endif
-
-ifeq ($(CONFIG_HAL_USB_CAMERA),y)
-SUBDIR_MAKEFILES += $(call all-named-subdir-makefiles,uvccam_test)
-SUBDIR_MAKEFILES += $(call all-named-subdir-makefiles,hal_camera_usb)
-SUBDIR_MAKEFILES += $(call all-named-subdir-makefiles,hal_camera_study_v4l2)
-endif
-
-ifeq ($(CONFIG_HAL_PARTED),y)
-SUBDIR_MAKEFILES += $(call all-named-subdir-makefiles,lidbg_parted)
-endif
-
 ifeq ($(CONFIG_HAL_VOLD_PX3),y)
 SUBDIR_MAKEFILES += $(call all-named-subdir-makefiles,px3_vold)
 endif
@@ -82,19 +97,6 @@ ifeq ($(CONFIG_HAL_VOLD_MT3561),y)
 SUBDIR_MAKEFILES += $(call all-named-subdir-makefiles,mt35x_vold)
 endif
 
-ifeq ($(CONFIG_HAL_USB_CAMERA),y)
-SUBDIR_MAKEFILES += $(call all-named-subdir-makefiles,libusb01/android/jni)
-endif
 
-ifeq ($(CONFIG_HAL_USB_CAMERA),y)
-SUBDIR_MAKEFILES += $(call all-named-subdir-makefiles,camera-fw-update)
-endif
 
-ifeq ($(CONFIG_HAL_BOOTANIMATION),y)
-SUBDIR_MAKEFILES += $(call all-named-subdir-makefiles,bootanimation)
-endif
-
-ifeq ($(CONFIG_HAL_BOOTANIMATION_6),y)
-SUBDIR_MAKEFILES += $(call all-named-subdir-makefiles,bootanimation_6.0)
-endif
 include $(SUBDIR_MAKEFILES)
