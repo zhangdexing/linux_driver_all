9887290c0c30ccad5377d7f8dc859fe1aba22bd9
lisuwei <lisuweizhai@126.com>
2017-01-07 21:19:32 +0800

    record_klogctl删除文件方式修正

--- app/record_klogctl/record_klogctl.c
+++ app/record_klogctl/record_klogctl.c
@@ -8,8 +8,66 @@
 int openfd;
 int readsize = 0,savesize = 0;
 char * buf = NULL;
-
+char newfile[128];
 void filesize_ctrl();
+
+
+int remove_old_file()
+{
+        DIR *d;
+        struct OLDERFILE{
+                long mtime;
+            	char filename[128];
+        }olderfile;
+        struct dirent *de;
+        char fname[128];
+        struct stat filebuf;
+        int findnum = 0;
+        d = opendir(PATH);
+        long mtime = 0;
+        if(d == NULL)
+        {
+                perror("prsize");
+                exit(1);
+        }
+	lidbg("remove_old_file\n");
+        while((de = readdir(d))!=NULL)
+        {
+		if(strncmp(de->d_name,".",1) == 0)
+                        continue;
+                sprintf(fname,"%s%s",PATH,de->d_name);
+		if(strcmp(fname,newfile) == 0)
+			continue;
+                int exists = stat(fname,&filebuf);
+                if(exists < 0)
+                {
+                        lidbg("Could not stat %s\n",de->d_name);
+                }
+                else
+                {
+                        if(findnum == 0)
+                        {
+                                olderfile.mtime = filebuf.st_mtime;
+                                sprintf(olderfile.filename,"%s%s",PATH,de->d_name);
+                                findnum ++;
+                        }
+                        else
+                        {
+                                if(filebuf.st_mtime<olderfile.mtime)
+                                {
+                                        olderfile.mtime = filebuf.st_mtime;
+                                        sprintf(olderfile.filename,"%s%s",PATH,de->d_name);
+                                }
+                                findnum ++;
+                        }
+                        lidbg("%s:file->st_mtime is%d\n",de->d_name,filebuf.st_mtime);
+                }
+        }      
+        lidbg("now remove %s\n",olderfile.filename);
+        remove(olderfile.filename);
+        return 0;
+}
+
 
 void write_log(char *buf)
 {
@@ -87,9 +145,10 @@ void filesize_ctrl()
 		else
 		{
 			//sprintf(cmd,"rm %s$(ls %s -rt | sed -n '1p')",PATH,PATH);
-			sprintf(cmd,"rm %s$(ls %s | sed -n '1p')",PATH,PATH);
-			system(cmd);
-			lidbg("record_klogctl:do %s\n",cmd);
+			//sprintf(cmd,"rm %s$(ls %s | sed -n '1p')",PATH,PATH);
+			//system(cmd);
+			//lidbg("record_klogctl:do %s\n",cmd);
+			remove_old_file();
 		}
 	}
 }
@@ -107,7 +166,6 @@ void sigfunc(int sig)
 
 int main()
 {
-	char newfile[128];
 	time_t ctime;
 	struct tm *tm;
 	
@@ -125,6 +183,7 @@ int main()
 	buf = (char *)malloc(BUFSIZE);
 	openfd = open(newfile,O_RDWR | O_CREAT,0777);
 	system("chmod 777 /data/lidbg/reckmsg/* ");
+
 	signal(SIGUSR1,sigfunc);
 	write_log(buf);
 	free(buf);
