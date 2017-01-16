73517a02e4b58b745f5de82fa96a2beea8e4f734
lisuwei <lisuweizhai@126.com>
2017-01-16 19:22:47 +0800

    3561优化打印;休眠保护白名单保护mtk工程模式

--- soc/mt35x/lidbg_io_mt35x.c
+++ soc/mt35x/lidbg_io_mt35x.c
@@ -150,10 +150,10 @@ int soc_io_config(u32 index, int func, u32 direction, u32 pull, u32 drive_streng
         err = gpio_request(index, "lidbg_io");
         if (err)
         {
-            lidbg("\n\nerr: gpio request failed1 %d!!!!!!\n\n\n",index);
+            lidbg("err: gpio request failed1 %d!!!!!!\n",index);
             gpio_free(index);
             err = gpio_request(index, "lidbg_io");
-            lidbg("\n\nerr: gpio request failed2 %d!!!!!!\n\n\n",index);
+            lidbg("err: gpio request failed2 %d!!!!!!\n",index);
         }
 
         if(direction == GPIO_CFG_INPUT)
