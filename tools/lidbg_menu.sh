ec23c3a90711c7d8fc33d9071a9cd47e84ef6aa7
futengfei <2220929658@qq.com>
2017-03-16 11:06:24 +0800

    优化脚本

--- tools/lidbg_menu.sh
+++ tools/lidbg_menu.sh
@@ -162,13 +162,12 @@ function lidbg_handle()
 		10)
 			echo cp -f $DBG_SYSTEM_DIR/out/target/product/$DBG_PLATFORM/$2 $DBG_OUT_PATH/$2
 			echo cp -f $DBG_SYSTEM_DIR/out/target/product/$DBG_PLATFORM/$2 /media/*/
-			echo cp -f $DBG_OUT_PATH/$2 /media/*/
 			echo adb push $DBG_SYSTEM_DIR/out/target/product/$DBG_PLATFORM/$2 /$2
 			echo adb push $DBG_OUT_PATH/$2 /system/bin/$2
 			echo adb shell "chmod 777 /system/bin/$2"
+			mkdir -p $(dirname $DBG_OUT_PATH/$2)
 			cp -f $DBG_SYSTEM_DIR/out/target/product/$DBG_PLATFORM/$2 $DBG_OUT_PATH/$2
 			cp -f $DBG_SYSTEM_DIR/out/target/product/$DBG_PLATFORM/$2 /media/*/
-			cp -f $DBG_OUT_PATH/$2 /media/*/
 			adb root
 			adb remount
 			adb push $DBG_SYSTEM_DIR/out/target/product/$DBG_PLATFORM/$2 /$2
