b11ea6b05ef2c74bc958d66c0806d14d53d53819
futengfei <540720599@qq.com>
2017-04-01 10:28:30 +0800

    开启红色OSD先保存PS进程信息到SD卡

--- drivers/misc_devices/misc_devices.c
+++ drivers/misc_devices/misc_devices.c
@@ -425,6 +425,7 @@ static void parse_cmd(char *pt)
         lidbg("acc_debug_mode enable!");
         g_var.is_debug_mode = 1;
 	 lidbg_shell_cmd("echo appcmd *158#077 > /dev/lidbg_drivers_dbg0");
+	 lidbg_shell_cmd("ps > /sdcard/ps_start.txt &");
     }
 
     else if (!strcmp(argv[0], "udisk_enable"))
