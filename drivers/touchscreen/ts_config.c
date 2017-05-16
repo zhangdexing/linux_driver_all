231bd2db8476099f1071aaa2e4c7ab7500ebe6fc
futengfei <540720599@qq.com>
2017-05-16 12:19:49 +0800

    添加弟兄8寸带虚拟按键(ID.42)

--- drivers/touchscreen/ts_config.c
+++ drivers/touchscreen/ts_config.c
@@ -48,6 +48,7 @@ u8 cfg_info_group38[] = CTP927_CFG_GROUP14;
 u8 cfg_info_group39[] = CTP_CFG_GROUP23;
 u8 cfg_info_group40[] = CTP927_CFG_GROUP15;
 u8 cfg_info_group41[] = CTP911_CFG_GROUP16;
+u8 cfg_info_group42[] = CTP_CFG_GROUP24;
 
 u8 *send_cfg_buf[] = {cfg_info_group1, cfg_info_group2,
                   cfg_info_group3, cfg_info_group4, cfg_info_group5, cfg_info_group6,
@@ -57,7 +58,7 @@ u8 *send_cfg_buf[] = {cfg_info_group1, cfg_info_group2,
                   cfg_info_group22, cfg_info_group23, cfg_info_group24, cfg_info_group25, cfg_info_group26, cfg_info_group27,
                   cfg_info_group28, cfg_info_group29,cfg_info_group30,
 		  cfg_info_group31, cfg_info_group32,cfg_info_group33,cfg_info_group34,cfg_info_group35,cfg_info_group36,
-		  cfg_info_group37, cfg_info_group38,cfg_info_group39,cfg_info_group40,cfg_info_group41
+		  cfg_info_group37, cfg_info_group38,cfg_info_group39,cfg_info_group40,cfg_info_group41,cfg_info_group42
                  };
 EXPORT_SYMBOL(send_cfg_buf);
 
@@ -102,6 +103,7 @@ u8 cfg_info_len[] = {CFG_GROUP_LEN(cfg_info_group1),
 		 CFG_GROUP_LEN(cfg_info_group39),
 		 CFG_GROUP_LEN(cfg_info_group40),
 		 CFG_GROUP_LEN(cfg_info_group41),
+		 CFG_GROUP_LEN(cfg_info_group42),
 
                 };
 
@@ -117,7 +119,7 @@ static int ts_config_init(void)
     DUMP_BUILD_TIME;
     if(0)
    {
-    	g_var.hw_info.ts_config = 41;
+    	g_var.hw_info.ts_config = 42;
     	g_var.hw_info.virtual_key = 0;
     }
     return 0;
