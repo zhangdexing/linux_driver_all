0d4739a19e8f632e1887173e72c6202252cd3227
futengfei <540720599@qq.com>
2017-06-03 16:27:10 +0800

    增加安卓标准第三方内置apk安装方法

---
+++ conf/init.flyaudio.preinstall.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+cd /flyapdata/install/
+if [ -s /data/app/preinstall.txt ]; then  
+    echo "3rddon't need to copy preinstall files" 
+    echo "3rddon't need to copy preinstall files"  > /dev/dbg_msg
+else  
+echo appcheck > /data/app/preinstall.txt 
+apklist="$(ls *.apk)"  
+for apkfile in ${apklist}; do  
+#skip equal apk
+if cmp  ${apkfile} /data/app/${apkfile} 
+then
+echo ==3rd=skip.copy===${apkfile} > /dev/dbg_msg
+echo ==3rd=skip.copy===${apkfile} 
+continue
+fi
+#copy
+echo ==3rd=copy===${apkfile} > /dev/dbg_msg
+echo ==3rd=copy===${apkfile} 
+dd if=${apkfile} of=/data/app/${apkfile}  
+    chmod 777 /data/app/${apkfile}  
+done 
+fi
+
