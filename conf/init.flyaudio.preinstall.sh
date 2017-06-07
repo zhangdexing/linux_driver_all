4940f414ce6b97b2a87c86bdf85558df4e888850
lisuwei <lisuweizhai@126.com>
2017-06-07 09:52:59 +0800

    Revert "增加安卓标准第三方内置apk安装方法-修改安装路径"
    This reverts commit 2920a39acd0521b404465a1a932c2273bd1059bc.

--- conf/init.flyaudio.preinstall.sh
+++ conf/init.flyaudio.preinstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /flyapdata/preinstall/
+cd /flyapdata/install/
 if [ -s /data/app/preinstall.txt ]; then  
     echo "3rddon't need to copy preinstall files" 
     echo "3rddon't need to copy preinstall files"  > /dev/dbg_msg
