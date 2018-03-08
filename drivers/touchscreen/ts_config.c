8ce17bd55abde7a0fec6ace87d48c8606de577e3
hdj <946973965@qq.com>
2018-03-08 15:36:47 +0800

    添加触摸屏9寸配置(1280*720)

--- drivers/touchscreen/ts_config.c
+++ drivers/touchscreen/ts_config.c
@@ -56,6 +56,7 @@ u8 cfg_info_group46[] = CTP911_CFG_GROUP17;
 u8 cfg_info_group47[] = CTP911_CFG_GROUP18;
 u8 cfg_info_group48[] = CTP928_CFG_GROUP17;
 u8 cfg_info_group49[] = CTP927_CFG_GROUP18;
+u8 cfg_info_group50[] = CTP_CFG_GROUP25;
 
 u8 *send_cfg_buf[] = {cfg_info_group1, cfg_info_group2,
                   cfg_info_group3, cfg_info_group4, cfg_info_group5, cfg_info_group6,
@@ -66,7 +67,8 @@ u8 *send_cfg_buf[] = {cfg_info_group1, cfg_info_group2,
                   cfg_info_group28, cfg_info_group29,cfg_info_group30,
 		  cfg_info_group31, cfg_info_group32,cfg_info_group33,cfg_info_group34,cfg_info_group35,cfg_info_group36,
 		  cfg_info_group37, cfg_info_group38,cfg_info_group39,cfg_info_group40,cfg_info_group41,cfg_info_group42,
-			cfg_info_group43, cfg_info_group44, cfg_info_group45, cfg_info_group46, cfg_info_group47, cfg_info_group48, cfg_info_group49
+			cfg_info_group43, cfg_info_group44, cfg_info_group45, cfg_info_group46, cfg_info_group47, cfg_info_group48, cfg_info_group49,
+			cfg_info_group50
                  };
 EXPORT_SYMBOL(send_cfg_buf);
 
@@ -119,6 +121,7 @@ u8 cfg_info_len[] = {CFG_GROUP_LEN(cfg_info_group1),
 		 CFG_GROUP_LEN(cfg_info_group47),
 		 CFG_GROUP_LEN(cfg_info_group48),
 		 CFG_GROUP_LEN(cfg_info_group49),
+		 CFG_GROUP_LEN(cfg_info_group50),
 
                 };
 
@@ -134,7 +137,7 @@ static int ts_config_init(void)
     DUMP_BUILD_TIME;
     if(0)
    {
-    	g_var.hw_info.ts_config = 48;
+    	g_var.hw_info.ts_config = 50;
     	g_var.hw_info.virtual_key = 0;
     }
     return 0;
