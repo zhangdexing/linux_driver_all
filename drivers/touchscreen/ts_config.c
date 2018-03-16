7666d5d8e470b43d1d9d888a52482348346de9e4
hdj <946973965@qq.com>
2018-03-16 14:13:03 +0800

    添加帝显9.7寸触摸屏配置

--- drivers/touchscreen/ts_config.c
+++ drivers/touchscreen/ts_config.c
@@ -58,6 +58,7 @@ u8 cfg_info_group48[] = CTP928_CFG_GROUP17;
 u8 cfg_info_group49[] = CTP927_CFG_GROUP18;
 u8 cfg_info_group50[] = CTP_CFG_GROUP25;
 u8 cfg_info_group51[] = CTP928_CFG_GROUP18;
+u8 cfg_info_group52[] = CTP928_CFG_GROUP19;
 
 u8 *send_cfg_buf[] = {cfg_info_group1, cfg_info_group2,
                   cfg_info_group3, cfg_info_group4, cfg_info_group5, cfg_info_group6,
@@ -69,7 +70,7 @@ u8 *send_cfg_buf[] = {cfg_info_group1, cfg_info_group2,
 		  cfg_info_group31, cfg_info_group32,cfg_info_group33,cfg_info_group34,cfg_info_group35,cfg_info_group36,
 		  cfg_info_group37, cfg_info_group38,cfg_info_group39,cfg_info_group40,cfg_info_group41,cfg_info_group42,
 			cfg_info_group43, cfg_info_group44, cfg_info_group45, cfg_info_group46, cfg_info_group47, cfg_info_group48, cfg_info_group49,
-			cfg_info_group50, cfg_info_group51
+			cfg_info_group50, cfg_info_group51, cfg_info_group52
                  };
 EXPORT_SYMBOL(send_cfg_buf);
 
@@ -124,6 +125,7 @@ u8 cfg_info_len[] = {CFG_GROUP_LEN(cfg_info_group1),
 		 CFG_GROUP_LEN(cfg_info_group49),
 		 CFG_GROUP_LEN(cfg_info_group50),
 		 CFG_GROUP_LEN(cfg_info_group51),
+		 CFG_GROUP_LEN(cfg_info_group52),
 
                 };
 
@@ -139,7 +141,7 @@ static int ts_config_init(void)
     DUMP_BUILD_TIME;
     if(0)
    {
-    	g_var.hw_info.ts_config = 51;
+    	g_var.hw_info.ts_config = 52;
     	g_var.hw_info.virtual_key = 0;
     }
     return 0;
