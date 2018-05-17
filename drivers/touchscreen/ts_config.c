3744f89b4be297460e99fd2eab6f7ae69b5d0970
hdj <946973965@qq.com>
2018-05-17 14:57:28 +0800

    添加弟兄触摸屏配置有区域按键(ID＝53 使用ts_config_25.conf)

--- drivers/touchscreen/ts_config.c
+++ drivers/touchscreen/ts_config.c
@@ -59,6 +59,7 @@ u8 cfg_info_group49[] = CTP927_CFG_GROUP18;
 u8 cfg_info_group50[] = CTP_CFG_GROUP25;
 u8 cfg_info_group51[] = CTP928_CFG_GROUP18;
 u8 cfg_info_group52[] = CTP928_CFG_GROUP19;
+u8 cfg_info_group53[] = CTP_CFG_GROUP26;
 
 u8 *send_cfg_buf[] = {cfg_info_group1, cfg_info_group2,
                   cfg_info_group3, cfg_info_group4, cfg_info_group5, cfg_info_group6,
@@ -70,7 +71,7 @@ u8 *send_cfg_buf[] = {cfg_info_group1, cfg_info_group2,
 		  cfg_info_group31, cfg_info_group32,cfg_info_group33,cfg_info_group34,cfg_info_group35,cfg_info_group36,
 		  cfg_info_group37, cfg_info_group38,cfg_info_group39,cfg_info_group40,cfg_info_group41,cfg_info_group42,
 			cfg_info_group43, cfg_info_group44, cfg_info_group45, cfg_info_group46, cfg_info_group47, cfg_info_group48, cfg_info_group49,
-			cfg_info_group50, cfg_info_group51, cfg_info_group52
+			cfg_info_group50, cfg_info_group51, cfg_info_group52, cfg_info_group53
                  };
 EXPORT_SYMBOL(send_cfg_buf);
 
@@ -126,6 +127,7 @@ u8 cfg_info_len[] = {CFG_GROUP_LEN(cfg_info_group1),
 		 CFG_GROUP_LEN(cfg_info_group50),
 		 CFG_GROUP_LEN(cfg_info_group51),
 		 CFG_GROUP_LEN(cfg_info_group52),
+		 CFG_GROUP_LEN(cfg_info_group53),
 
                 };
 
@@ -141,7 +143,7 @@ static int ts_config_init(void)
     DUMP_BUILD_TIME;
     if(0)
    {
-    	g_var.hw_info.ts_config = 52;
+    	g_var.hw_info.ts_config = 53;
     	g_var.hw_info.virtual_key = 0;
     }
     return 0;
